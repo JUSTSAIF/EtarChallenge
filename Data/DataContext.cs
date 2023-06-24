06371b26b6c9ec992ec654a7eae4f7b4f563736f
JUSTSAIF <saifalcigeary@gmail.com>
2023-06-24 14:13:27 +0300

    add ConnectionStrings, DataContext and registered the context

---
+++ Data/DataContext.cs
@@ -0,0 +1,10 @@
+namespace EtarChallenge.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
